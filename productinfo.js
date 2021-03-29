04fe4f3dca2b41f3861a1ed5fcc681433365f4bd
emjay357 <harynguyen357@gmail.com>
2021-03-30 00:39:26 +0700

    update

---
+++ productinfo.js
@@ -0,0 +1,11 @@
+var fanname = "LG Smart Inverter™ Tủ lạnh ngăn đá trên (Đen)";
+function product(name){
+  productfix = name;
+}
+product(fanname)
+function productfixname(productfix2){
+  document.getElementById("kaka").innerHTML = productfix2;
+}
+$(document).ready(function(){
+  productfixname(productfix);
+})
